340f630009506a115a57ce24fa098fcf653dc672
Anton Kanin <antony.kanin@gmail.com>
2022-12-08 15:13:31 +0300

    making sure the working dir is set to the scrip location

--- copy_server_libs.cmd
+++ copy_server_libs.cmd
@@ -1,2 +1,3 @@
+cd ..\..\..\treasurehunters-client-unity
 copy ..\treasurehunters-server\NtlStudio.TreasureHunters.Server\NtlStudio.TreasureHunters.Model\bin\Debug\netstandard2.1\*.dll src\Library\PackageCache\com.ntlstudio.treasurehunters.model@1.0.5\Assemblies\
 copy ..\treasurehunters-server\NtlStudio.TreasureHunters.Server\NtlStudio.TreasureHunters.Model\bin\Debug\netstandard2.1\*.pdb src\Library\PackageCache\com.ntlstudio.treasurehunters.model@1.0.5\Assemblies\
