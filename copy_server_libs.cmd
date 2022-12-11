0d9d07358022659837701e34ddeca6730efc9768
Andrei Kuleuski <kuljok@gmail.com>
2022-12-11 23:12:41 +0300

    Remove copy_server_libs as the copying of libs are done on the server side

--- copy_server_libs.cmd
+++
@@ -1,7 +0,0 @@
-cd ..\..\..\treasurehunters-client-unity
-
-copy ..\treasurehunters-server\NtlStudio.TreasureHunters.Server\NtlStudio.TreasureHunters.Model\bin\Debug\netstandard2.1\*.dll %localappdata%\Unity\cache\packages\npm.pkg.github.com\@ntl-studio\com.ntlstudio.treasurehunters.model@1.0.5\Assemblies
-copy ..\treasurehunters-server\NtlStudio.TreasureHunters.Server\NtlStudio.TreasureHunters.Model\bin\Debug\netstandard2.1\*.pdb %localappdata%\Unity\cache\packages\npm.pkg.github.com\@ntl-studio\com.ntlstudio.treasurehunters.model@1.0.5\Assemblies
-
-copy ..\treasurehunters-server\NtlStudio.TreasureHunters.Server\NtlStudio.TreasureHunters.Model\bin\Debug\netstandard2.1\*.dll src\Library\PackageCache\com.ntlstudio.treasurehunters.model@1.0.5\Assemblies\
-copy ..\treasurehunters-server\NtlStudio.TreasureHunters.Server\NtlStudio.TreasureHunters.Model\bin\Debug\netstandard2.1\*.pdb src\Library\PackageCache\com.ntlstudio.treasurehunters.model@1.0.5\Assemblies\
