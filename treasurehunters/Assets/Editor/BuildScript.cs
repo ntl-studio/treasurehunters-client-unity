c56b47f52ce9e61c95e5d355ffbd87b047221d5f
Andrei Kuleuski <kuljok@gmail.com>
2023-01-01 19:43:32 +0300

    Remove hardcoded locationPathName from the BuildScript

--- treasurehunters/Assets/Editor/BuildScript.cs
+++ treasurehunters/Assets/Editor/BuildScript.cs
@@ -1,12 +1,26 @@
+using System;
 using UnityEditor;
 
 public static class BuildScript 
 {
     public static void PerformBuild()
     {
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
-        buildPlayerOptions.locationPathName = "~/projects/ntl-studio/ios";
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = new[] { "Assets/Scenes/SampleScene.unity" }
+        };
+        var args = System.Environment.GetCommandLineArgs();
+        var foundBuildPath = false;
+        for (var i = 0; i < args.Length; i++)
+        {
+            if (args[i] != "-buildPath") continue;
+            buildPlayerOptions.locationPathName = args[i + 1];
+            foundBuildPath = true;
+        }
+        if (!foundBuildPath)
+        {
+            throw new ArgumentException("Please specify a build path with -buildPath argument");
+        }
         buildPlayerOptions.target = BuildTarget.iOS;
         BuildPipeline.BuildPlayer(buildPlayerOptions);
     }
