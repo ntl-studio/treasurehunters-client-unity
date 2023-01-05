cc257e9fe387f822da4ff236601b731d10d6aa03
Anton Kanin <antony.kanin@gmail.com>
2023-01-05 14:13:23 +0300

    Using async/await instead of unity coroutines - done

--- treasurehunters/Assets/ServerConnection.cs
+++ treasurehunters/Assets/ServerConnection.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using JsonObjects;
 using TreasureHunters;
 using UnityEngine;
-using UnityEngine.Networking;
 
 using NtlStudio.TreasureHunters.Model;
 
@@ -28,6 +26,9 @@ public class ServerConnection : MonoBehaviour
     private static ServerConnection _instance;
     private static GameClient Game => GameClient.Instance();
     private string ServerAddress => Game.ServerName;
+    private enum RequestType { Get, Post, Put }
+
+    private static readonly HttpClient _httpClient = new();
 
     public static ServerConnection Instance()
     {
@@ -44,14 +45,11 @@ public class ServerConnection : MonoBehaviour
         return _instance;
     }
 
-    public void CreateGameAsync(Action gameListCallback)
+    public async void CreateGameAsync(Action gameListCallback)
     {
         string uri = $"https://{ServerAddress}/api/v1/games/create?privateGame=false";
-
-        StartCoroutine(WebRequest<NewGameDataJson>(uri, (_) => 
-            { gameListCallback(); },
-            RequestType.Post
-        ));
+        await WebRequestAsync<NewGameDataJson>(uri, RequestType.Post);
+        gameListCallback();
     }
 
     public async Task<GamesJson> UpdateGamesListAsync()
@@ -63,200 +61,115 @@ public class ServerConnection : MonoBehaviour
 
     public delegate void JoinGameCallbackAction(bool joined, string gameId, string sessionId);
 
-    public void JoinGameAsync(string gameId, JoinGameCallbackAction joinGameCallback)
+    public async void JoinGameAsync(string gameId, JoinGameCallbackAction joinGameCallback)
     {
         string uri = $"https://{ServerAddress}/api/v1/games/{gameId}/players/{Game.PlayerName}";
-
-        StartCoroutine(WebRequest<PlayersDataJson>(uri, (playerDataJson) => 
-            { joinGameCallback(playerDataJson.successful, gameId, playerDataJson.data.sessionid); },
-            RequestType.Put
-        ));
+        var playerData = await WebRequestAsync<PlayersDataJson>(uri, RequestType.Put);
+        joinGameCallback(playerData.successful, gameId, playerData.data.sessionid);
     }
 
-    public void GetTreasurePositionAsync(string gameId, Action<int, int> getTreasurePositionCallback)
+    public async void GetTreasurePositionAsync(string gameId, Action<int, int> getTreasurePositionCallback)
     {
         string uri = $"https://{ServerAddress}/api/v1/games/{gameId}/treasureposition_debug";
-
-        StartCoroutine(WebRequest<TreasurePositionDataJson>(uri, (treasurePositionDataJson) =>
-            { getTreasurePositionCallback(treasurePositionDataJson.data.x, treasurePositionDataJson.data.y); },
-            RequestType.Get
-        ));
+        var treasureData = await WebRequestAsync<TreasurePositionDataJson>(uri, RequestType.Get);
+        getTreasurePositionCallback(treasureData.data.x, treasureData.data.y);
     }
 
-    public void GetGameStateAsync(string gameId, Action<string, int> gameStateCallback)
+    public async void GetGameStateAsync(string gameId, Action<string, int> gameStateCallback)
     {
         string uri = $"https://{ServerAddress}/api/v1/games/{gameId}";
-
-        StartCoroutine(WebRequest<GameDataJson>(uri, (gameDataJson) =>
-            { gameStateCallback(gameDataJson.data.state, gameDataJson.data.playerscount); },
-            RequestType.Get
-        ));
+        var gameData = await WebRequestAsync<GameDataJson>(uri, RequestType.Get);
+        gameStateCallback(gameData.data.state, gameData.data.playerscount);
     }
 
-    public void StartGameAsync(string gameId, Action startGameCallback)
+    public async void StartGameAsync(string gameId, Action startGameCallback)
     {
         string uri = $"https://{ServerAddress}/api/v1/games/{gameId}/start";
-
-        StartCoroutine(WebRequest<GameStateDataJson>(uri, (_) =>
-            { startGameCallback(); },
-            RequestType.Put
-        ));
+        await WebRequestAsync<GameStateDataJson>(uri, RequestType.Put);
+        startGameCallback();
     }
 
-    public void DeleteGameAsync(string gameId, Action deleteGameCallback)
+    public async void DeleteGameAsync(string gameId, Action deleteGameCallback)
     {
         string uri = $"https://{ServerAddress}/api/v1/games/{gameId}/delete";
-
-        StartCoroutine(WebRequest<DeleteGameDataJson>(uri, (_) =>
-            { deleteGameCallback(); },
-            RequestType.Post
-        ));
+        await WebRequestAsync<DeleteGameDataJson>(uri, RequestType.Post);
+        deleteGameCallback();
     }
 
-    public void GetCurrentPlayerAsync(string gameId, Action<string, string> currentPlayerCallback)
+    public async void GetCurrentPlayerAsync(string gameId, Action<string, string> currentPlayerCallback)
     {
         string uri = $"https://{ServerAddress}/api/v1/games/{gameId}/currentplayer";
-
-        StartCoroutine(WebRequest<CurrentPlayerDataJson>(uri, (currentPlayerJson) =>
-            { currentPlayerCallback(
-                currentPlayerJson.data.name,
-                currentPlayerJson.data.gamestate); },
-            RequestType.Get
-        ));
+        var currentPlayerJson = await WebRequestAsync<CurrentPlayerDataJson>(uri, RequestType.Get);
+        currentPlayerCallback(currentPlayerJson.data.name, currentPlayerJson.data.gamestate);
     }
 
-    public void GetPlayerInfoAsync(string gameId, string playerName, 
-        Action<int, int, int[]> getPlayerInfoCallback)
+    public async void GetPlayerInfoAsync(string gameId, string playerName, Action<int, int, int[]> getPlayerInfoCallback)
     {
         string uri = $"https://{ServerAddress}/api/v1/games/{gameId}/players/{playerName}";
-
-        StartCoroutine(WebRequest<PlayerInfoDataJson>(uri, (playerInfo) =>
-            { getPlayerInfoCallback(playerInfo.data.x, playerInfo.data.y, playerInfo.data.visiblearea); },
-            RequestType.Get
-        ));
+        var playerInfo = await WebRequestAsync<PlayerInfoDataJson>(uri, RequestType.Get);
+        getPlayerInfoCallback(
+            playerInfo.data.x,
+            playerInfo.data.y,
+            playerInfo.data.visiblearea
+        );
     }
 
-    public void PerformActionAsync(string gameId, string playerName, PlayerAction playerAction, 
+    public async void PerformActionAsync(string gameId, string playerName, PlayerAction playerAction, 
         Action<bool, bool, string> performActionCallback)
     {
         string uri = $"https://{ServerAddress}/api/v1/games/{gameId}/performaction/player/{playerName}/action/{playerAction.Type}/{playerAction.Direction}";
-
-        StartCoroutine(WebRequest<PlayerActionResultDataJson>(uri, (playerActionResult) => 
-            { 
-                performActionCallback(
-                    playerActionResult.successful, 
-                    playerActionResult.data.hastreasure,
-                    playerActionResult.data.state // game state
-                );
-            },
-            RequestType.Put
-        ));
+        var playerActionResult = await WebRequestAsync<PlayerActionResultDataJson>(uri, RequestType.Put);
+        performActionCallback(
+            playerActionResult.successful,
+            playerActionResult.data.hastreasure,
+            playerActionResult.data.state // game state
+        );
     }
 
-    public void GetWinnerAsync(string gameId, Action<string> getWinnerCallback)
+    public async void GetWinnerAsync(string gameId, Action<string> getWinnerCallback)
     {
         string uri = $"https://{ServerAddress}/api/v1/games/{gameId}/getwinner";
-
-        StartCoroutine(WebRequest<WinnerNameDataJson>(uri, (getWinnerResult) =>
-            {
-                getWinnerCallback(getWinnerResult.data);
-            },
-            RequestType.Get
-        ));
+        var winnerResult= await WebRequestAsync<WinnerNameDataJson>(uri, RequestType.Get);
+        getWinnerCallback(winnerResult.data);
     }
 
-    public void GetMovesHistoryAsync(string gameId, Action<List<PlayerMovesDetails>> movesHistoryCallback)
+    public async void GetMovesHistoryAsync(string gameId, Action<List<PlayerMovesDetails>> movesHistoryCallback)
     {
         string uri = $"https://{ServerAddress}/api/v1/games/{gameId}/playersmovestates";
+        var playersMoveStates = await WebRequestAsync<PlayersMoveHistoryDataJson>(uri, RequestType.Get);
 
-        StartCoroutine(WebRequest<PlayersMoveHistoryDataJson>(uri, (playersMoveStates) =>
+        List<PlayerMovesDetails> playerMoveStates = new();
+
+        foreach (var player in playersMoveStates.data.players)
+        {
+            PlayerMovesDetails details = new PlayerMovesDetails
             {
-                List<PlayerMovesDetails> playerMoveStates = new();
+                PlayerName = player.player
+            };
 
-                foreach (var player in playersMoveStates.data.players)
+            Debug.Assert(player.actionstates != null);
+
+            foreach (var playerAction in player.actionstates)
+            {
+                details.Moves.Add(new PlayerActionState()
                 {
-                    PlayerMovesDetails details = new PlayerMovesDetails
+                    Position = playerAction.position,
+
+                    Action = new PlayerAction
                     {
-                        PlayerName = player.player
-                    };
+                        Direction = playerAction.direction,
+                        Type = playerAction.type
+                    },
 
-                    Debug.Assert(player.actionstates != null);
-
-                    foreach (var playerAction in player.actionstates)
-                    {
-                        details.Moves.Add(new PlayerActionState()
-                        {
-                            Position = playerAction.position,
-
-                            Action = new PlayerAction
-                            {
-                                Direction = playerAction.direction,
-                                Type = playerAction.type
-                            },
-
-                            FieldCell = playerAction.cell
-                        });
-                    }
-
-                    playerMoveStates.Add(details);
-                }
-
-                movesHistoryCallback(playerMoveStates);
-            },
-            RequestType.Get
-        ));
-    }
-
-    enum RequestType { Get, Post, Put }
-
-    private IEnumerator WebRequest<T>(string uri, Action<T> callback, RequestType requestType)
-    {
-        Debug.Log($"Sending {requestType} request: {uri}");
-
-        UnityWebRequest request;
-
-        switch (requestType)
-        {
-            case RequestType.Get:
-                request = UnityWebRequest.Get(uri);
-                break;
-            case RequestType.Post:
-                request = UnityWebRequest.Post(uri, String.Empty);
-                break;
-            case RequestType.Put:
-                request = UnityWebRequest.Put(uri, String.Empty);
-                break;
-            default:
-                throw new Exception($"Not supported request type {requestType}");
-        }
-
-        var before = Time.realtimeSinceStartup;
-        yield return request.SendWebRequest();
-        var total = Time.realtimeSinceStartup - before;
-        Debug.Log($"WebRequest duration {total} for {uri}");
-
-        if (request.result == UnityWebRequest.Result.Success)
-        {
-            var jsonText = request.downloadHandler.text;
-
-            var data = JsonUtility.FromJson<T>(jsonText);
-            if (data == null)
-            {
-                Debug.Log($"Could not read data form the json: {jsonText}");
-                Debug.Assert(true);
+                    FieldCell = playerAction.cell
+                });
             }
-            else
-                Debug.Log($"Getting data: {jsonText}");
 
-            callback(data);
+            playerMoveStates.Add(details);
         }
-        else
-        {
-            Debug.Log($"{requestType} request {uri} failed with error: {request.error}");
-        }
+
+        movesHistoryCallback(playerMoveStates);
     }
-
-    private static readonly HttpClient _httpClient = new();
 
     private async Task<T> WebRequestAsync<T>(string uri, RequestType requestType)
     {
