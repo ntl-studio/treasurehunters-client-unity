fe1b1a1f2af6054150e993947df67f3a12b486b3
Anton Kanin <antony.kanin@gmail.com>
2023-01-07 10:55:34 +0300

    Enabling GC

--- treasurehunters/Assets/DisableGC.cs
+++ treasurehunters/Assets/DisableGC.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
-using UnityEngine.Scripting;
 
 public class DisableGC : MonoBehaviour
 {
+    private const bool _disableGC = false;
+
     void Start()
     {
+        if (_disableGC)
+        {
 #if !UNITY_EDITOR
-        Debug.Log("Disabling GC");
-        GarbageCollector.GCMode = GarbageCollector.Mode.Disabled;
+            Debug.Log("Disabling GC");
+            GarbageCollector.GCMode = GarbageCollector.Mode.Disabled;
 #endif
+        }
     }
 }
